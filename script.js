c3f0a4f737b24c9fa5bf739f27eb696f33e7b107
Shubhamsahar <kumarshubham765484@gmail.com>
2025-07-30 15:32:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,30 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+    const obj = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
 
-  //your code here
-  let result = '';
-    let values = Object.values(obj).reverse();
+    let result = '';
 
-    for (let i = 0; i < values.length; i++) {
-        let count = Math.floor(num / values[i][1]);
-        result += values[i][0].repeat(count);
-        num -= count * values[i][1];
+    for (let i = 0; i < obj.length; i++) {
+        while (num >= obj[i][1]) {
+            result += obj[i][0];
+            num -= obj[i][1];
+        }
     }
-
- result = result.replace('DCCCC', 'CM'); // 900
-    result = result.replace('CCCC', 'CD'); // 400
-    result = result.replace('LXXXX', 'XC'); // 90
-    result = result.replace('XXXX', 'XL'); // 40
-    result = result.replace('VIIII', 'IX'); // 9
-    result = result.replace('IIII', 'IV'); // 4
 
     return result;
 }
 
-
-
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
- console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+console.log(convertToRoman(36));
