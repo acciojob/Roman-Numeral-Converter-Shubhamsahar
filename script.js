fef12ac862c5eca0260255100010be6188176c21
Shubhamsahar <kumarshubham765484@gmail.com>
2025-08-04 23:17:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
-document.getElementById('convert').addEventListener('click', () => {
-    const number = parseInt(document.getElementById('number').value);
-    const result = convertToRoman(number);
-    document.getElementById('result').innerText = result;
-});
-
 function convertToRoman(num) {
-    const romanNumerals = [
-        ['M', 1000],
-        ['CM', 900],
-        ['D', 500],
-        ['CD', 400],
-        ['C', 100],
-        ['XC', 90],
-        ['L', 50],
-        ['XL', 40],
-        ['X', 10],
-        ['IX', 9],
-        ['V', 5],
-        ['IV', 4],
-        ['I', 1]
-    ];
+  if (num <= 0 || num > 100000) {
+    return "Invalid input";
+  }
 
-    let result = '';
+  const romanNumerals = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
-    for (let i = 0; i < romanNumerals.length; i++) {
-        const [symbol, value] = romanNumerals[i];
-        while (num >= value) {
-            result += symbol;
-            num -= value;
-        }
+  let result = "";
+
+  for (let [roman, value] of romanNumerals) {
+    while (num >= value) {
+      result += roman;
+      num -= value;
     }
+  }
 
-    return result;
+  return result;
 }
 
+function convert() {
+  const input = document.getElementById("number").value;
+  const output = document.getElementById("output");
+
+  const num = parseInt(input);
+
+  if (isNaN(num)) {
+    output.innerText = "Please enter a valid number.";
+    return;
+  }
+
+  const roman = convertToRoman(num);
+  output.innerText = roman;
+}
